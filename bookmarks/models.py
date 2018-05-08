222e558916c4b8f57702d17dd5590493547fed1d
Julian Kohlman <juliankohlman@gmail.com>
2018-05-08 17:32:58 -0400

    Create sample bookmarks

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,5 +1,16 @@
+from uuid import uuid4
 from django.db import models
 
 # Create your models here.
 class Bookmark(models.Model):
-  pass
+  """ Bookmarks model """
+  # id -- pk
+  id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  # url
+  url = models.URLField('URL', unique=True)
+  # name
+  name = models.CharField(max_length=200)
+  # tag 
+
+  # description/notes
+  description = models.TextField(blank=True)

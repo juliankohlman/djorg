69c160d9bb1852ce9ca86384b99964a024aa5448
Julian Kohlman <juliankohlman@gmail.com>
2018-05-11 12:55:17 -0400

    config debugging

--- djorg/settings.py
+++ djorg/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
+import os
 from decouple import config
 import django_heroku
 import dj_database_url
-import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,9 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # 'vj1w(b6xyl71(xewvdg*9d2^00$ke@a22n7fb&4t%(9y9@p$-j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition

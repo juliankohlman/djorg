3a08d7f72e397aff6b7ed54cdd35512ca16ceb65
Julian Kohlman <juliankohlman@gmail.com>
2018-05-14 18:03:50 -0400

    debugging on heroku

--- djorg/settings.py
+++ djorg/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool, default=False)
+DEBUG = True
+# DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = ['.herokuapp.com', '.localhost']
 

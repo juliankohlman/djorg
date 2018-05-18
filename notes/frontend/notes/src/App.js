25f9489013ba73600126927912a8b9860b602ae2
Julian Kohlman <juliankohlman@gmail.com>
2018-05-17 20:00:42 -0400

    frontend semi-connected

--- notes/frontend/notes/src/App.js
+++ notes/frontend/notes/src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    notes: []
+  };
+
+  // async componentDidMount() {
+  //   try {
+  //     const res = await fetch('http://127.0.0.1:8000/api/');
+  //     const notes = await res.json();
+  //     this.setState({notes})
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/api/')
+      .then(res => {
+        const notes = res.data;
+        this.setState({ notes });
+      })
+  }
+  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ol>
+        { this.state.notes.map(note => 
+          <li>
+            <h2>{note.title}</h2>
+              <p>{note.content}</p>
+          </li>)}
+      </ol>
     );
   }
 }
